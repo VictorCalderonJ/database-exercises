98fb75ba471b9e45d8dc3e6efc01336140d99251
victor calderon <calderon.j.victor@gmail.com>
2023-08-18 09:50:48 -0500

    test

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+    DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    release_date INT NOT NULL,
+--     sales DECIMAL(7 ,2) NOT NULL,
+    sales FLOAT UNSIGNED NOT NULL,
+    genre VARCHAR(150) NOT NULL ,
+    PRIMARY KEY (id)
+    );
